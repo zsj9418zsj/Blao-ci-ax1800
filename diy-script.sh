42e09025eacae2d5c9323fc295d76956570e8b86
zsj9418zsj <140706155+zsj9418zsj@users.noreply.github.com>
2024-10-19 20:18:55 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.180/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 修改默认名字
+sed -i "s/hostname='ImmortalWrt'/hostname='AX1800-Pro'/g" package/base-files/files/bin/config_generate
 
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
